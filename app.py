541fade9499b5cb9bda63614632a9f6c8c62e465
qweasd122 <tmdrnjsdl71@gmail.com>
2025-06-26 00:23:44 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def ask():
     return Response(stream_response(), content_type='text/plain')
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
